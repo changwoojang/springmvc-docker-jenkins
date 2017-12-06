5e3147ae1272aa580ea81e7cf60aaa56705bca7a
Changwoo Jang <changwooj111@gmail.com>
2017-12-06 22:42:56 +0900

    fix : POM 1.0 -> 1.1

---
+++ src/main/java/testing.java
@@ -0,0 +1,8 @@
+/**
+ * Created by changwooj111@gmail.com on 2017. 12. 6.
+ * Blog : http://woo-repository.tistory.com/
+ * Github : https://github.com/changwoojang
+ */
+public class testing {
+
+}
