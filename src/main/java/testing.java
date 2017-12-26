205701da8b64c5b08643640cfd5b4c4229af9ee5
Changwoo Jang <changwooj111@gmail.com>
2017-12-26 11:57:07 +0900

    testing

--- src/main/java/testing.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by changwooj111@gmail.com on 2017. 12. 6.
- * Blog : http://woo-repository.tistory.com/
- * Github : https://github.com/changwoojang
- */
-public class testing {
-
-}
